b558e1ff9a9b2976d85cbeedf57594d8022d9539
Krasimir Kolev <krasimir.kolev@softwaregroup.com>
2020-04-15 18:05:47 +0300

    test: coverage

--- test/encoding.test.js
+++ test/encoding.test.js
@@ -27,3 +27,14 @@ tap.test('encode message object without field 11 to buffer - test context', (t) 
     t.same(context, obj.contextWithoutField11, 'test meta object');
     t.end();
 });
+iso8583.fieldFormat.footer = {size: 8, name: 'Footer', format: 'string'};
+iso8583.fieldFormat.header = {size: 8, name: 'Header', format: 'string'};
+tap.test('encode message object to buffer - all fields + header +  footer', (t) => {
+    const meta = {};
+    const context = {};
+    const log = {};
+    t.same(iso8583.encode(obj.allFields, meta, context, log), Buffer.from('00'.repeat(8) + buff.allFields + '00'.repeat(8), 'hex'), 'encode all fields');
+    t.same(meta, obj.metaAllFields, 'test meta object');
+    t.same(context, obj.empty, 'test context object');
+    t.end();
+});
